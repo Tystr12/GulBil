5a5a2ac2c74e5fc53352ddae0929f10713770895
Tystr12 <73970359+Tystr12@users.noreply.github.com>
2023-07-19 11:09:53 +0200

    leaderboard fixes

--- App.tsx
+++ App.tsx
@@ -27,7 +27,7 @@ import {
 import {useState, useEffect, useCallback, useMemo} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {Header} from 'react-native-elements/dist/header/Header';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons.js';
 import {
   app,
   db,
@@ -45,6 +45,7 @@ import {
   arrayUnion,
   setDoc,
 } from 'firebase/firestore';
+import {lightColors} from '@rneui/base';
 
 /** This is a react native component, where you can pass a title and children.     */
 const Stack = createStackNavigator();
@@ -174,64 +175,53 @@ function HomeScreen({navigation}): JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    paddingHorizontal: 4,
-    marginBottom: 16,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    padding: 60,
-    position: 'relative',
-    backgroundColor: 'black',
-  },
-  gulBilButton: {
-    backgroundColor: 'yellow',
-    padding: 8,
-    borderRadius: 8,
-  },
-  button: {
-    backgroundColor: 'yellow',
-    padding: 8,
-    borderRadius: 8,
-  },
-  bottomLeftButton: {
-    alignSelf: 'flex-start',
-    marginBottom: 16,
-  },
-  bottomRightButton: {
-    alignSelf: 'flex-end',
-    marginBottom: 16,
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  text: {
-    color: 'yellow',
-    fontSize: 20,
-  },
-  highlight: {
-    color: '#42f2f5',
-    fontSize: 30,
-  },
-});
-
 function LeaderBoard({navigation}): JSX.Element {
+  const [livePresses, setLivePresses] = useState(0);
+  const [liveTimestamps, setLiveTimestamps] = useState<String[]>([]);
+  const [tyPresses, setTyPresses] = useState(0);
+  const [tyTimestamps, setTyTimestamps] = useState<String[]>([]);
+  const [dates, setDates] = useState<Date[]>([]);
+
+  useEffect(() => {
+    const getDatabasePresses = async () => {
+      const docRefLive = doc(db, 'Users', 'Live');
+      const docSnapLive = await getDoc(docRefLive);
+
+      const docRefTy = doc(db, 'Users', 'Ty');
+      const docSnapTy = await getDoc(docRefTy);
+
+      if (docSnapLive.exists() && docSnapTy.exists()) {
+        const livePresses = docSnapLive.data().total;
+        const liveTotal = docSnapLive.data().biler;
+        const tyPresses = docSnapTy.data().total;
+        const tyTotal = docSnapTy.data().biler;
+
+        setLivePresses(livePresses);
+        setLiveTimestamps(liveTotal.map(timestamp => timestamp.toString()));
+        setTyPresses(tyPresses);
+        setTyTimestamps(tyTotal.map(timestamp => timestamp.toString()));
+      } else {
+        console.log('docSnap data was empty...');
+      }
+    };
+
+    getDatabasePresses();
+  }, []);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>LeaderBoard</Text>
-      <Button
-        title="Go to LeaderBoard... again"
-        onPress={() => navigation.navigate('LeaderBoard')}
-      />
+    <View style={styles.leaderBoardContainer}>
       <Button title="Home" onPress={() => navigation.navigate('GulBil')} />
+      <Text style={styles.text}>
+        Leader:{' '}
+        {tyPresses >= livePresses ? (
+          <Text style={styles.highlight}>Ty</Text>
+        ) : (
+          <Text style={styles.highlight}>Live</Text>
+        )}
+        <Icon name="crown-outline" size={30} color={'yellow'} />
+      </Text>
+      <Text style={styles.text}>Live: {livePresses}</Text>
+      <Text style={styles.text}>Ty: {tyPresses}</Text>
     </View>
   );
 }
@@ -260,5 +250,61 @@ function App({}): JSX.Element {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 4,
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: 60,
+    position: 'relative',
+    backgroundColor: 'black',
+  },
+  leaderBoardContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyAlign: 'center',
+    backgroundColor: 'black',
+  },
+  gulBilButton: {
+    backgroundColor: 'yellow',
+    padding: 8,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: 'yellow',
+    padding: 6,
+    borderRadius: 8,
+  },
+  bottomLeftButton: {
+    alignSelf: 'flex-start',
+    marginBottom: 16,
+  },
+  bottomRightButton: {
+    alignSelf: 'flex-end',
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'yellow',
+    fontSize: 20,
+  },
+  highlight: {
+    color: '#42f2f5',
+    fontSize: 30,
+  },
+});
 
 export default App;
